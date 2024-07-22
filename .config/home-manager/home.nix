79710d38e2f6d4dc6bf39f72887ae354b98fb76d
Luis David Licea Torres <luislicea1011@hotmail.com>
2024-07-21 23:23:27 -0500

    Home-Manager: Remove comments

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -15,33 +15,23 @@
   # release notes.
   home.stateVersion = "24.05"; # Please read the comment before changing.
 
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
   home.packages = with pkgs; [
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
+    (pkgs.nerdfonts.override { fonts = [ "FiraCode" ]; })
     bat
     bat-extras.batman
     brave
     castor
     ffmpeg
+    font-awesome
     glide-media-player
     gnome.polari
     helix
     nil
-    sshfs
-    vlc
-
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    (pkgs.nerdfonts.override { fonts = [ "FiraCode" ]; })
-    font-awesome
     noto-fonts
     noto-fonts-cjk
     noto-fonts-emoji
+    sshfs
+    vlc
 
     # # You can also create simple shell scripts directly inside your
     # # configuration. For example, this adds a command 'my-hello' to your
@@ -89,19 +79,12 @@
     };
   };
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
   home.file = {
     # # Building this configuration will create a copy of 'dotfiles/screenrc' in
     # # the Nix store. Activating the configuration will then make '~/.screenrc' a
     # # symlink to the Nix store copy.
     # ".screenrc".source = dotfiles/screenrc;
 
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
     ".config/nvim/init.lua".text = ''
         --------------------------------------------------------------------------------
         -- Essential Mappings.
